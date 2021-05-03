3e755a8c9605a76396d6c4d692a056b5b0f64431
Hikmet Tutuncu <48223977+htutuncu@users.noreply.github.com>
2021-05-03 14:51:54 +0300

    Delete MernisValidation directory

--- MernisValidation/src/Abstract/BaseCustomerManager.java
+++
@@ -1,9 +0,0 @@
-package Abstract;
-
-import Entities.Customer;
-
-public abstract class BaseCustomerManager implements CustomerService{
-	public void save(Customer customer) {
-		System.out.println("Saved to db." + customer.getFirstName());
-	}
-}
--- MernisValidation/src/Concrete/NeroCustomerManager.java
+++
@@ -1,7 +0,0 @@
-package Concrete;
-
-import Abstract.BaseCustomerManager;
-
-public class NeroCustomerManager extends BaseCustomerManager{
-	
-}
